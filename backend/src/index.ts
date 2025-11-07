c24fb913e0565f43f2e892b55bbfcf87f1458edc
Anuj8171 <anujrana9264@gmail.com>
2025-11-07 17:23:35 +0530

    update3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,24 @@
 import express, { Request, Response } from "express";
-// import cookieParser from "cookie-parser";
-
 import path from "path";
 import authRouter from "./routes/auth.route";
 import productRouter from "./routes/product.routes";
 import cors from "cors";
 
-
 const app = express();
-const port = 5000;
+
+
+const port = process.env.PORT || 5000;
+
 app.use(cors({
-  credentials:true
-}))
+  credentials: true
+}));
+
 app.use("/images", express.static(path.join(__dirname, "../public/images")));
-// app.use(cookieParser());
 app.use(express.json());
-app.use('/api/auth',authRouter);
-app.use('/api/product',productRouter);
+
+app.use('/api/auth', authRouter);
+app.use('/api/product', productRouter);
+
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
